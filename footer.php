290ceea62cca859b3d4345c5e99ca8c6e49b59ed
Antoine Kuntz <79264035+AntoineKuntz@users.noreply.github.com>
2021-06-04 11:35:46 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<div id = "separation">
-    <hr style="height: 2px; color: #000000; background-color: #000000; width: 80%; border: none;"> 
-</div>
-    <a href="https://www.facebook.com/onepieceofficial"><img src="./assets/fblogo.png" width="40px"/></a>
-    <a href="https://www.instagram.com/onepiece_staff/?hl=fr"><img src="./assets/instalogo.png" width="40px"/></a>
-<p> Crédit : Selma, Antoine et Mélissa</p>
-</footer>
